4e510d812382be991e97bbb869d23c4a80d8b493
beihu235 <105789304+beihu235@users.noreply.github.com>
2023-07-09 09:30:06 +0800

    Add files via upload

--- source/Controls.hx
+++ source/Controls.hx
@@ -434,6 +434,7 @@ class Controls extends FlxActionSet
 		inline forEachBound(Control.NOTE_DOWN, (action, state) -> addButtonNOTES(action, Hitbox.buttonDown, state));
 		inline forEachBound(Control.NOTE_LEFT, (action, state) -> addButtonNOTES(action, Hitbox.buttonLeft, state));
 		inline forEachBound(Control.NOTE_RIGHT, (action, state) -> addButtonNOTES(action, Hitbox.buttonRight, state));
+		inline forEachBound(Control.SPACE, (action, state) -> addbuttonUI(action, Hitbox.buttonSpace, state));
 	}
 	
 	public function setNewHitBox(Hitbox:FlxNewHitbox)
